3e796870e26819061462712aff7d9c05fde6c6e1
Алим Невилов <nevilplay@mail.ru>
2021-01-29 19:38:57 +0300

    Added postgre db

---
+++ backend/DaraAds.Infrastructure/DaraAdsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaraAds.Infrastructure
+{
+    public class DaraAdsDbContext: DbContext
+    {
+        public DaraAdsDbContext(DbContextOptions<DaraAdsDbContext> options) : base(options) { }
+
+    }
+}
