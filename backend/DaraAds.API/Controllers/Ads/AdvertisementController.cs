48ffb8b6adf40f1331939ea6b8616a905a3ce926
Алим Невилов <nevilplay@mail.ru>
2021-01-31 18:16:15 +0300

    Changed name controller

---
+++ backend/DaraAds.API/Controllers/Ads/AdvertisementController.cs
@@ -0,0 +1,30 @@
+﻿using DaraAds.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DaraAds.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaraAds.API.Controllers.Ads
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdsController : ControllerBase
+    {
+        private readonly DaraAdsDbContext _context;
+
+        public AdsController(DaraAdsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Advertisement>>> GetAds()
+        {
+            return await _context.Advertisements.ToListAsync();
+        }
+        
+    }
+}
