1ff78fe4c5fc7dc25c0104fdc299a23e5099b622
Алим Невилов <nevilplay@mail.ru>
2021-01-31 15:20:12 +0300

    Changed Adv entity and added migration

--- backend/DaraAds.Core/Entities/Advertisement.cs
+++ backend/DaraAds.Core/Entities/Advertisement.cs
@@ -8,8 +8,17 @@ namespace DaraAds.Core.Entities
 {
     public class Advertisement : BaseEntity
     {
+        public string Title { get; set; }
+
         public string Description { get; set; }
 
         public decimal Price { get; set; }
+
+        public string Cover { get; set; }
+
+        public string Category { get; set; }
+
+        public string SubCategory { get; set; }
+
     }
 }
---
+++ backend/DaraAds.Infrastructure/Migrations/20210131121934_Update.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DaraAds.Infrastructure.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Advertisements",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cover",
+                table: "Advertisements",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubCategory",
+                table: "Advertisements",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Advertisements",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "Cover",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "SubCategory",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Advertisements");
+        }
+    }
+}
