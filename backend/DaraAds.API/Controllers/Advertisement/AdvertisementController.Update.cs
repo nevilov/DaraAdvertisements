b4d9821e276b959f0d9e600d34429368d1a5cc2b
Алим Невилов <nevilplay@mail.ru>
2021-02-15 00:29:01 +0300

    Added update advertisement function #67

--- backend/DaraAds.API/Controllers/Advertisement/AdvertisementController.Update.cs
+++ backend/DaraAds.API/Controllers/Advertisement/AdvertisementController.Update.cs
@@ -1,8 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DaraAds.Application.Services.Advertisement.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using DaraAds.Application.Services.Advertisement.Contracts;
+using System.ComponentModel.DataAnnotations;
 
 namespace DaraAds.API.Controllers.Advertisement
 {
     public partial class AdvertisementController : ControllerBase
     {
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(
+            CancellationToken cancellationToken,
+            [FromServices] IAdvertisementService service,
+            [FromRoute] int id,
+            AdvertisementUpdateRequest request)
+        {
+            var response = await service.Update(new Update.Request
+            {
+                Id = id,
+                Title = request.Title,
+                Description = request.Description,
+                Price = request.Price,
+                Cover = request.Cover
+            }, cancellationToken);
+
+            return Ok(response.Id);
+
+        }
+    }
+
+    public sealed class AdvertisementUpdateRequest
+    {
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(10000)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 100_000_000_000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Cover { get; set; }
     }
 }
--- backend/DaraAds.Application/Services/Advertisement/Contracts/Update.cs
+++ backend/DaraAds.Application/Services/Advertisement/Contracts/Update.cs
@@ -6,16 +6,25 @@ namespace DaraAds.Application.Services.Advertisement.Contracts
     {
         public sealed class Request
         {
+            public int Id { get; set; }
             public string Title { get; set; }
             public string Description { get; set; }
             public decimal Price { get; set; }
             public string Cover { get; set; }
+            public Statuses Status { get; set; }
             public DateTime UpdateDate = DateTime.UtcNow;
         }
         
         public sealed class Response
         {
             public int Id { get; set; }
+        }
+
+        public enum Statuses
+        {
+            Created,
+            Payed,
+            Closed
         }
     }
 }
