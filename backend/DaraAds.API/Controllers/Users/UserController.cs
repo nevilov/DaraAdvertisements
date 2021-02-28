dde4ec54edf223b6ab8b4f11e16c3c739162867b
Алим Невилов <nevilplay@mail.ru>
2021-02-28 13:07:57 +0000

    Revert "Configured IdentityService #138"

--- backend/DaraAds.API/Controllers/Users/UserController.cs
+++ backend/DaraAds.API/Controllers/Users/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using DaraAds.Application.Services.User.Interfaces;
+using System.Threading;
+using DaraAds.Application.Services.User.Contracts;
 using DaraAds.Application.Identity.Interfaces;
 
 namespace DaraAds.API.Controllers.Users
@@ -17,6 +20,44 @@ namespace DaraAds.API.Controllers.Users
         {
             _userService = userService;
             _identityService = identityService;
-        }     
+        }
+
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Register(
+            [FromBody] UserRegisterRequest request,
+            CancellationToken cancellationToken)
+        {
+            var registrationResult = await _userService.Register(new Register.Request
+            {
+                Username = request.Username,
+                Name = request.Name,
+                LastName = request.LastName,
+                Email = request.Email,
+                Phone = request.Phone,
+                Password = request.Password
+            }, cancellationToken);
+
+            return Created($"api/v1/users/{registrationResult.UserId}", new { });
+        }        
+    }
+    
+    public sealed class UserRegisterRequest
+    {
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Имя пользователя - обязательно")]
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email пользователя - обязательно")]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        [MaxLength(30)]
+        [MinLength(6)]
+        public string Password { get; set; }
     }
 }
---
+++ backend/DaraAds.API/Controllers/Users/UserExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using DaraAds.API.Dto.Users;
+using DaraAds.Domain;
+
+namespace DaraAds.API.Controllers.Users
+{
+    public static class UserExtension
+    {
+        public static UsersListItemDto ToDto(this User user)
+        {
+            return new UsersListItemDto
+            {
+//                Id = user.Id,
+                Name = user.Name,
+                LastName = user.LastName,
+                Avatar = user.Avatar,
+                Phone = user.Phone,
+                Email = user.Email,
+            };
+        }
+
+        public static UsersListItemDto ToDto(this ClaimsPrincipal principal)
+        {
+            return new()
+            {
+                Id = int.Parse(principal.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value),
+                Name = principal.Claims.First(c => c.Type == ClaimTypes.Name).Value
+            };
+        }
+    }
+}
