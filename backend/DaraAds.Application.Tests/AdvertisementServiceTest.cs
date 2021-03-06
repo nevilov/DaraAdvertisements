7738279bd73e8556423bad6f57ba0a8619dae8a3
viktor.kostylev <viktor.kostylev@mail.ru>
2021-03-06 11:55:32 +0300

    Added tests for Advertisement Creation

--- backend/DaraAds.Application.Tests/AdvertisementServiceTest.cs
+++ backend/DaraAds.Application.Tests/AdvertisementServiceTest.cs
@@ -1,7 +1,13 @@
+using AutoFixture.Xunit2;
 using DaraAds.Application.Identity.Interfaces;
 using DaraAds.Application.Repositories;
+using DaraAds.Application.Services.Advertisement.Contracts;
 using DaraAds.Application.Services.Advertisement.Implementations;
 using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace DaraAds.Application.Tests
 {
@@ -17,6 +23,49 @@ namespace DaraAds.Application.Tests
             _identityServiceMock = new Mock<IIdentityService>();
 
             advertisementService = new AdvertisementService(_advertisementRepositoryMock.Object, _identityServiceMock.Object);
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task Create_Response_Success(
+            Create.Request request, CancellationToken cancellationToken,
+            int userId, int adId)
+        {
+            ConfigureMoqEnvironment(userId.ToString(), adId);
+
+            // Act
+            var response = await advertisementService.Create(request, cancellationToken);
+
+            // Assert
+            _identityServiceMock.Verify();
+            Assert.NotNull(response);
+            Assert.NotEqual(default, response.Id);
+        }
+
+
+        [Theory]
+        [InlineAutoData(null)]
+        public async Task Throw_Exception_If_Request_Failure(
+            Create.Request request, CancellationToken cancellationToken,
+            int userId, int adId)
+        {
+            ConfigureMoqEnvironment(userId.ToString(), adId);
+
+            // Assert
+            await Assert.ThrowsAsync<NullReferenceException>(async () => await advertisementService.Create(request, cancellationToken));
+            _identityServiceMock.Verify();
+        }
+
+        private void ConfigureMoqEnvironment(string userId, int adId)
+        {
+            _identityServiceMock
+                .Setup(_ => _.GetCurrentUserId(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(userId)
+                .Verifiable();
+
+            _advertisementRepositoryMock
+                .Setup(_ => _.Save(It.IsAny<Domain.Advertisement>(), It.IsAny<CancellationToken>()))
+                .Callback((Domain.Advertisement ad, CancellationToken cancellationToken) => ad.Id = adId);
         }
 
     }
