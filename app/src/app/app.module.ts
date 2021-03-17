96bcf1f26d7d7e8042f31913adc4e18c0dbeba57
bogdana-badyanova-dev <badyanova.bogdana@gmail.com>
2021-03-17 21:55:26 +0300

    Add MenuComponent #156

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { MenuComponent } from './@shared/components/menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
