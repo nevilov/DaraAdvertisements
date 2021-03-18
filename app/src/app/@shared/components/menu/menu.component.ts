3652192f78c301e53ae144c74a1d2dd703cb97f5
bogdana-badyanova-dev <badyanova.bogdana@gmail.com>
2021-03-18 20:12:21 +0300

    Add submenu #157

--- app/src/app/@shared/components/menu/menu.component.ts
+++ app/src/app/@shared/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { SubmenuComponent } from './../submenu/submenu.component';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./../../../../assets/scss/layout/__menu.scss']
 })
 export class MenuComponent implements OnInit {
+
+  @ViewChild("submenu", { read: ElementRef }) SubmenuComponent!: ElementRef;
+  @ViewChild("btnShowsSubmenu", { read: ElementRef }) span!: ElementRef;
+  isSubmenuShown: boolean = false;
+
+  public showSubmenu() {
+    if (this.isSubmenuShown) {
+      this.SubmenuComponent.nativeElement.style.display = 'none';
+      this.span.nativeElement.style.transform = "rotate(180deg)";
+      this.isSubmenuShown = false;
+    } else {
+      this.SubmenuComponent.nativeElement.style.display = 'block';
+      this.span.nativeElement.style.transform = "rotate(360deg)";
+      this.isSubmenuShown = true;
+    }
+
+
+
+    // this.tref.nativeElement.textContent
+    // this.tref.nativeElement.textContent
+    // document.querySelector("app-submenu").setAttribute("style", "background:red; dispplay: block;");
+  }
 
   constructor() { }
 
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
 import { MenuComponent } from './@shared/components/menu/menu.component';
+import { SubmenuComponent } from './@shared/components/submenu/submenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SubmenuComponent
   ],
   imports: [
     BrowserModule,
