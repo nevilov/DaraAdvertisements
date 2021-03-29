8edbc4277a4c78adfd60a371f9ed9f38da629cc4
bogdana-badyanova-dev <badyanova.bogdana@gmail.com>
2021-03-29 17:15:38 +0300

    Add advertisementPage methods

--- app/src/app/layout/advertisement/advertisementPage/advertisementPage.component.ts
+++ app/src/app/layout/advertisement/advertisementPage/advertisementPage.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Advertisement } from 'src/app/Dtos/advertisement';
+import { AdvertisementService } from 'src/app/services/advertisements.service';
 
 @Component({
   selector: 'app-advertisementPage',
   templateUrl: './advertisementPage.component.html',
-  styleUrls: ['./../../../../assets/scss/pages/__home.scss']
+  styleUrls: ['./../../../../assets/scss/pages/__home.scss'],
+  providers: [
+    AdvertisementService
+  ]
 })
 export class AdvertisementPageComponent implements OnInit {
 
-  constructor() { }
+    advertisements: Advertisement[] = [];               // массив объяв
 
-  ngOnInit() {
-  }
+    constructor(private advertisementService: AdvertisementService) { }
 
+    ngOnInit() {
+        this.loadProducts();    // загрузка данных при старте компонента
+    }
+
+    // получаем данные через сервис
+    loadProducts() {
+        this.advertisementService.getAllAdvertisements()
+            .subscribe(data => {
+              this.advertisements = data.items;
+              console.log(this.advertisements)
+            });
+    }
 }
