a980e81f0390566a56cf26c1c0f5a64102fe5883
Алим Невилов <nevilplay@mail.ru>
2021-03-29 21:23:33 +0300

    Commented validation fields for work register

--- backend/DaraAds.API/Dto/Users/Validators/UserRegisterRequestValidator.cs
+++ backend/DaraAds.API/Dto/Users/Validators/UserRegisterRequestValidator.cs
@@ -21,17 +21,17 @@ namespace DaraAds.API.Dto.Users.Validators
                 .Matches("[0-9]").WithMessage("Пароль должен содержать цифры")
                 .Matches("[^a-zA-Z0-9]").WithMessage("Пароль должен содержать особый символы");
 
-            RuleFor(x => x.Name)
-                .NotEmpty().WithMessage("Имя не может быть пустым")
-                .MaximumLength(MAX_LENGTH).WithMessage("Имя не может быть больше 100 символов");
+            //RuleFor(x => x.Name)
+            //    .NotEmpty().WithMessage("Имя не может быть пустым")
+            //    .MaximumLength(MAX_LENGTH).WithMessage("Имя не может быть больше 100 символов");
             
-            RuleFor(x => x.LastName)
-                .NotEmpty().WithMessage("Фамилия не может быть пустым")
-                .MaximumLength(MAX_LENGTH).WithMessage("Фамилия не может быть больше 100 символов");
+            //RuleFor(x => x.LastName)
+            //    .NotEmpty().WithMessage("Фамилия не может быть пустым")
+            //    .MaximumLength(MAX_LENGTH).WithMessage("Фамилия не может быть больше 100 символов");
 
-            RuleFor(x => x.Phone)
-                .NotEmpty().WithMessage("Номер телефона не может быть пустым")
-                .MaximumLength(MAX_LENGTH).WithMessage("Номер телефона не может быть более 100 символов");
+            //RuleFor(x => x.Phone)
+            //    .NotEmpty().WithMessage("Номер телефона не может быть пустым")
+            //    .MaximumLength(MAX_LENGTH).WithMessage("Номер телефона не может быть более 100 символов");
         }
     }
 }
