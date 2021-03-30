cd3aad3f7d1ee747d0bc697fdaac615c963707b6
bogdana-badyanova-dev <badyanova.bogdana@gmail.com>
2021-03-30 19:20:00 +0300

    Add advertisement preview

--- app/src/app/shared/components/advertisement/advertisement.component.ts
+++ app/src/app/shared/components/advertisement/advertisement.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-advertisement',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advertisement.component.scss']
 })
 export class AdvertisementComponent implements OnInit {
+
+  @Input() title: string = "";
+  @Input() price: number = 0;
+  @Input() ownerName: string = "";
 
   constructor() { }
 
