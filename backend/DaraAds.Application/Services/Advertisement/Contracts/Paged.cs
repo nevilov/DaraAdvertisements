fb39f9226cf823abff4c73f7ef7a61a73d3e4594
Алим Невилов <nevilplay@mail.ru>
2021-04-03 15:24:11 +0300

    Add method get user advertisements #212

---
+++ backend/DaraAds.Application/Services/Advertisement/Contracts/Paged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaraAds.Application.Services.Advertisement.Contracts
+{
+    public class Paged
+    {
+        public int Limit { get; set; } = 10;
+        public int Offset { get; set; } = 0;
+    }
+}
