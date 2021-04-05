d9cb58875b937f991cf6f7f7fdf54c7edee17f0f
Алим Невилов <nevilplay@mail.ru>
2021-04-05 19:15:45 +0300

    Fix Lastname register

--- backend/DaraAds.Application/Services/Advertisement/Contracts/GetPages.cs
+++ backend/DaraAds.Application/Services/Advertisement/Contracts/GetPages.cs
@@ -16,7 +16,7 @@ namespace DaraAds.Application.Services.Advertisement.Contracts
                 public string Id { get; set; }
                 public string Email { get; set; }
                 public string Name { get; set; }
-                public string LastName { get; set; }
+                public string Lastname { get; set; }
                 public string Username { get; set; }
                 public IEnumerable<ImageResponse> Images { get; set; }
             }
