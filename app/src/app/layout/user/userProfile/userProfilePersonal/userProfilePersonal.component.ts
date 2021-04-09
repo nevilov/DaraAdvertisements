fc459d1d1a2976f854f7a75878b26ecebc207f51
bogdana-badyanova-dev <badyanova.bogdana@gmail.com>
2021-04-09 20:26:18 +0300

    Small fix

--- app/src/app/layout/user/userProfile/userProfilePersonal/userProfilePersonal.component.ts
+++ app/src/app/layout/user/userProfile/userProfilePersonal/userProfilePersonal.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-userProfilePersonal',
   templateUrl: './userProfilePersonal.component.html',
-  styleUrls: ['./userProfilePersonal.component.scss']
+  styleUrls: ['./userProfilePersonal.component.scss'],
 })
 export class UserProfilePersonalComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  avatarChange() {
+    const fileInput = document.getElementById('avatar');
+    fileInput?.addEventListener('change', function () {
+      const selectedFile = fileInput;
+      alert();
+    });
   }
-
 }
