05c826fef0557c51ddf6209c35cc83f56d79fc9f
bogdana-badyanova-dev <badyanova.bogdana@gmail.com>
2021-04-11 12:11:56 +0300

    Add changing password

--- app/src/app/layout/user/userProfile/userProfileAdvertisements/userProfileAdvertisements.component.ts
+++ app/src/app/layout/user/userProfile/userProfileAdvertisements/userProfileAdvertisements.component.ts
@@ -1,15 +1,39 @@
+import { UserService } from 'src/app/services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Advertisement } from 'src/app/Dtos/advertisement';
+import { AdvertisementService } from 'src/app/services/advertisements.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
-  selector: 'app-userProfileAdvertisements',
-  templateUrl: './userProfileAdvertisements.component.html',
-  styleUrls: ['./userProfileAdvertisements.component.scss']
+	selector: 'app-userProfileAdvertisements',
+	templateUrl: './userProfileAdvertisements.component.html',
+	styleUrls: ['./userProfileAdvertisements.component.scss'],
 })
 export class UserProfileAdvertisementsComponent implements OnInit {
+	advertisements: Advertisement[] = [];
 
-  constructor() { }
+	userName: string = '';
+	constructor(
+		private userService: UserService,
+		private cookieService: CookieService
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.userName = this.cookieService.get('AuthUsername');
+		this.loadUserInfo();
+	}
 
+	loadAdvertisements(id: string) {
+		this.userService.getUserAdvertisements(id).subscribe((data) => {
+			this.advertisements = data.items;
+			console.log(data);
+		});
+	}
+
+	loadUserInfo() {
+		this.userService.getUser(this.userName).subscribe((data) => {
+			this.loadAdvertisements(data.id);
+			console.log(data);
+		});
+	}
 }
