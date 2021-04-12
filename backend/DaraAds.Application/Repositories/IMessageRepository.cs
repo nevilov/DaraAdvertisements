8798581160d43e862a4c92ecbae2ff7a7cd050a0
Алим Невилов <nevilplay@mail.ru>
2021-04-12 13:36:49 +0300

    Add MessageRepository #261

---
+++ backend/DaraAds.Application/Repositories/IMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using DaraAds.Domain;
+using System.Threading.Tasks;
+
+namespace DaraAds.Application.Repositories
+{
+    public interface IMessageRepository : IRepository<Message, long>
+    {
+        Task<Message[]> FindByAdvertisementAndUsers(int advertisementId, string ownerId, string customerId = null);
+    }
+}
---
+++ backend/DaraAds.Infrastructure/DataAccess/Repositories/MessageRepository.cs
@@ -0,0 +1,28 @@
+﻿using DaraAds.Application.Repositories;
+using DaraAds.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaraAds.Infrastructure.DataAccess.Repositories
+{
+    public class MessageRepository : Repository<Message, long>, IMessageRepository
+    {
+        public MessageRepository(DaraAdsDbContext _context) : base(_context)
+        {
+        }
+
+        public Task<Message[]> FindByAdvertisementAndUsers(int advertisementId, string ownerId, string customerId = null)
+        {
+            var data =  _context.Messages
+                .AsNoTracking()
+                .Where(m => m.AdvertisementId == advertisementId && m.CustomerId == customerId);
+
+            if (customerId != null)
+            {
+                data.Where(m => m.CustomerId == customerId);
+            }
+            return data.ToArrayAsync();
+        }
+    }
+}
