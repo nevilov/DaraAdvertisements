4bdcdb00f90cea28e730153d3e109a7dcea22df6
Алим Невилов <nevilplay@mail.ru>
2021-04-24 09:43:52 +0300

    Add block user method #279

---
+++ backend/DaraAds.API/Controllers/Users/UserController.Block.cs
@@ -0,0 +1,20 @@
+﻿using DaraAds.Application.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DaraAds.API.Controllers.Users
+{
+    public partial class UserController
+    {
+        [HttpPost("block/{userId}")]
+        [Authorize(Roles = "Moderator, Admin")]
+        public async Task<IActionResult> BlockUser(string userId, DateTime untillDate, CancellationToken cancellationToken)
+        {
+            var result = await _identityService.BlockUser(userId, untillDate, cancellationToken);
+            return Ok();
+        }
+    }
+}
