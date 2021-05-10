d60abeeaaf27c825e24acdad503f8eb606dc87db
Alim Nevilov <nevilplay@mail.ru>
2021-05-10 11:34:45 +0300

    Configure files for deploy

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 	title = 'DaraAds';
-	public static backendAddress: string = 'https://localhost:5001';
+	public static backendAddress: string = 'http://localhost:80';
 }
